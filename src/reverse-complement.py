1c7176943bea18b467e9024a6560fb0c3a08e22b
DaiannaGP <daianna@lcg.unam.mx>
2021-03-04 12:05:39 -0600

    Adición de sequence.txt y cambio de reverse_complement.py

---
+++ src/reverse-complement.py
@@ -0,0 +1,27 @@
+'''
+NAME
+	reverse-complement.py
+    
+VERSION
+    1.0
+    
+AUTHOR
+	
+    
+DESCRIPTION
+	Make the reverse complement of DNA sequence
+        I modified this
+    
+CATEGORY
+	Genomic Sequence
+    
+USAGE
+
+    % python reverse-complement.py -i 
+    
+    example
+    
+    % python reverse-complement -i sequence.txt
+
+'''
+
